8dd0058321bc8f678fe7258d0e6a882aba48e5b4
Bill Zhang <207559339@qq.com>
2019-08-01 15:12:46 +0800

    starting template

---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomeScreen from './screens/HomeScreen'
+
+class App extends React.Component{
+    render(){
+        return ( <div>
+            <HomeScreen />
+        </div>
+        )
+    }
+}
+
+export default App
